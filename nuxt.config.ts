1687ae503f26f0a707d59bbcb3bb86debc45fa9f
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-19 02:35:52 +0700

    build(deps): install @nuxt/image-edge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "nuxt-icon",
+    "@nuxt/image-edge",
+  ],
   googleFonts: {
     families: {
       Inter: {
