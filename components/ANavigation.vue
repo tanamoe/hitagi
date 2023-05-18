7a9684ffa8a95b50845086120e6cc2cdc92ada41
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-19 01:30:09 +0700

    feat: load navigation from Ghost setting

--- components/ANavigation.vue
+++ components/ANavigation.vue
@@ -1,12 +1,25 @@
+<script setup lang="ts">
+const { data } = await useFetch(`/api/setting`);
+</script>
+
 <template>
   <nav class="sticky top-0 z-50 h-full w-full bg-zinc-100/75 backdrop-blur-md">
-    <div class="container mx-auto flex items-center gap-3 px-3">
-      <NuxtLink href="/" class="px-3 py-3">
+    <div
+      class="container mx-auto flex items-center gap-3 overflow-x-scroll px-3"
+    >
+      <NuxtLink href="/" class="flex-shrink-0 px-3 py-3">
         <img src="/logo.svg" class="h-6" />
       </NuxtLink>
-      <NuxtLink href="/" class="px-3 py-3">
-        <span>Phỏng vấn</span>
-      </NuxtLink>
+      <div v-if="data && data.navigation" class="flex items-center gap-3">
+        <NuxtLink
+          v-for="item in data.navigation"
+          :key="item.url"
+          :href="item.url"
+          class="whitespace-nowrap px-3 py-3 font-bold transition-all"
+        >
+          <span>{{ item.label }}</span>
+        </NuxtLink>
+      </div>
     </div>
   </nav>
 </template>
