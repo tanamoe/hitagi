bf8c96831080825ca8076985d25bb89eda5cf9f7
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-19 02:04:13 +0700

    feat(ui): implement custom error page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+
+useSeoMeta({
+  title: `${props.error.statusMessage} - Tana Blog`,
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="flex h-[90vh] flex-col items-center justify-center gap-6">
+      <h1 class="font-heading text-8xl">{{ error.statusCode }}</h1>
+      <p>Đã có một lỗi xảy ra: {{ error.statusMessage }}</p>
+      <button
+        class="bg-primary p-3 transition-shadow hover:shadow-offset hover:shadow-black"
+        @click="handleError"
+      >
+        Trở về trang chủ
+      </button>
+    </div>
+  </NuxtLayout>
+</template>
+
+<style>
+html {
+  @apply bg-zinc-100 text-zinc-800;
+}
+</style>
