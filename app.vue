9ebdaffb6c56a991ba3151506dfbc4569320b3d2
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-20 23:28:17 +0700

    feat: introduce page transition

--- app.vue
+++ app.vue
@@ -30,6 +30,55 @@ useSeoMeta({
 
 <style>
 html {
-  @apply bg-zinc-100 text-zinc-800;
+  @apply overflow-x-hidden bg-zinc-100 text-zinc-800;
+}
+
+.page-enter-active,
+.page-leave-active {
+  @apply transition-all duration-500;
+}
+
+.page-enter-active::before,
+.page-leave-active::before {
+  @apply fixed inset-0 z-40 bg-primary transition-transform duration-500;
+  content: "";
+}
+
+.page-enter-from::before {
+  @apply translate-x-0;
+}
+
+.page-enter-to::before {
+  @apply translate-x-full;
+}
+
+.page-leave-from::before {
+  @apply -translate-x-full;
+}
+
+.page-leave-to::before {
+  @apply translate-x-0;
+}
+
+.page-enter-active::after,
+.page-leave-active::after {
+  @apply fixed inset-0 z-50 bg-zinc-100 transition-transform delay-200 duration-300;
+  content: "";
+}
+
+.page-enter-from::after {
+  @apply translate-x-0;
+}
+
+.page-enter-to::after {
+  @apply translate-x-full;
+}
+
+.page-leave-from::after {
+  @apply -translate-x-full;
+}
+
+.page-leave-to::after {
+  @apply translate-x-0;
 }
 </style>
