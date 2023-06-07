330c63b594b13cb05cf0dbf4f193902e309c90d9
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-06-07 22:49:57 +0700

    refactor: cleanup

--- components/AFooter.vue
+++ components/AFooter.vue
@@ -1,7 +1,3 @@
-<script setup>
-const year = new Date().getFullYear();
-</script>
-
 <template>
   <div class="container mx-auto p-6">
     <hr class="mb-6 h-3 w-24 border-none bg-zinc-400" />
@@ -41,15 +37,17 @@ const year = new Date().getFullYear();
           <a
             class="transition-colors duration-100 ease-linear hover:text-zinc-700"
             href="/sitemap.xml"
-            >Sitemap</a
           >
+            Sitemap
+          </a>
         </li>
         <li>
           <a
             class="transition-colors duration-100 ease-linear hover:text-zinc-700"
             href="mailto:konnichiwa@tana.moe"
-            >Liên hệ</a
           >
+            Liên hệ
+          </a>
         </li>
       </ul>
     </div>
